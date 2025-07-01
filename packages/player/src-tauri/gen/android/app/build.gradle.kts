3b88786a50f5b873b0ae80402552d0ac4a8b61a7
apoint123 <108002475+apoint123@users.noreply.github.com>
2025-07-01 18:18:17 +0800

    CI: 更新缓存 Player: 更新安卓构建的依赖版本

--- packages/player/src-tauri/gen/android/app/build.gradle.kts
+++ packages/player/src-tauri/gen/android/app/build.gradle.kts
@@ -14,13 +14,13 @@ val tauriProperties = Properties().apply {
 }
 
 android {
-    compileSdk = 35
+    compileSdk = 36
     namespace = "net.stevexmh.amllplayer"
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
         applicationId = "net.stevexmh.amllplayer"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
     }
@@ -62,12 +62,12 @@ rust {
 }
 
 dependencies {
-    implementation("androidx.webkit:webkit:1.6.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.webkit:webkit:1.14.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.8.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
 
 apply(from = "tauri.build.gradle.kts")
--- packages/player/src-tauri/gen/android/app/src/main/java/net/stevexmh/amllplayer/MainActivity.kt
+++ packages/player/src-tauri/gen/android/app/src/main/java/net/stevexmh/amllplayer/MainActivity.kt
@@ -1,6 +1,7 @@
 package net.stevexmh.amllplayer
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
@@ -10,20 +11,49 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.view.WindowCompat
 
 class MainActivity : TauriActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    WindowCompat.setDecorFitsSystemWindows(window, false)
-    if (Build.VERSION.SDK_INT >= 30) {
-      if (!Environment.isExternalStorageManager()) {
-        AlertDialog.Builder(this).setTitle(R.string.all_files_access_required_title)
-          .setMessage(R.string.all_files_access_required_text)
-          .setPositiveButton(R.string.all_files_access_required_go_to_setting) { _, _ ->
-            startActivity(Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION))
-          }.setNegativeButton(R.string.all_files_access_required_ignore) { _, _ ->
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        setupImmersiveUi()
 
-          }.setCancelable(true).create().show()
-      }
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
-    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-  }
+
+    override fun onResume() {
+        super.onResume()
+
+        checkAllFilesAccessPermission()
+    }
+
+    private fun setupImmersiveUi() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        val controller = WindowCompat.getInsetsController(window, window.decorView)
+        controller.isAppearanceLightStatusBars = false
+        controller.isAppearanceLightNavigationBars = false
+    }
+
+    private fun checkAllFilesAccessPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager()) {
+                AlertDialog.Builder(this)
+                    .setTitle(R.string.all_files_access_required_title)
+                    .setMessage(R.string.all_files_access_required_text)
+                    .setPositiveButton(R.string.all_files_access_required_go_to_setting) { _, _ ->
+                        try {
+                            val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+                            intent.addCategory("android.intent.category.DEFAULT")
+                            intent.data = Uri.parse("package:${applicationContext.packageName}")
+                            startActivity(intent)
+                        } catch (e: Exception) {
+                            val intent = Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
+                            startActivity(intent)
+                        }
+                    }
+                    .setNegativeButton(R.string.all_files_access_required_ignore, null)
+                    .setCancelable(true)
+                    .show()
+            }
+        }
+    }
 }
