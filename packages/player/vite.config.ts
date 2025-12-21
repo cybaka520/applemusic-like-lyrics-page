bbea6e91f6c74a19ae1e58a5f45e00e6da7309c0
apoint123 <108002475+apoint123@users.noreply.github.com>
2025-12-22 00:53:10 +0800

    chore(deps): 更新依赖

--- packages/player/vite.config.ts
+++ packages/player/vite.config.ts
@@ -133,10 +133,10 @@ export default defineConfig({
 	// 1. prevent vite from obscuring rust errors
 	clearScreen: false,
 	// 2. tauri expects a fixed port, fail if that port is not available
-	server: {
-		port: 1420,
-		strictPort: true,
-	},
+	// server: {
+	// 	port: 1420,
+	// 	strictPort: true,
+	// },
 	// 3. to make use of `TAURI_DEBUG` and other env variables
 	// https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
 	envPrefix: ["VITE_"],
